dbeb1a06c95b5a3ad990ec6f6ce616314d871782
Juan Santelices <86135408+jdsantelicesl@users.noreply.github.com>
2024-09-19 09:20:56 -0700

    added gitignore

---
+++ pygame_ex.py
@@ -0,0 +1,31 @@
+# This example was taken straight from the pygame docs, feel free to play around with it.
+# The best way to get familiar with a new language/library is to play around with code and
+# frequently visit its official documentation: https://www.pygame.org/docs/
+
+# Example file showing a basic pygame "game loop"
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((1280, 720))
+clock = pygame.time.Clock()
+running = True
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("purple")
+
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
